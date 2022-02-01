8603e7e4fa13167c0a24771d22c8a706715c286b
JoshMeijer <36229349+JoshMeijer@users.noreply.github.com>
2022-02-01 13:56:53 +0200

    Remove debug from Git

--- SC/GraniteDatabaseCustom/GraniteDatabaseCustom/bin/Debug/Process/PrintShipLabels/Create.sql
+++
@@ -1,9 +0,0 @@
-:setvar root "\Process\PrintShipLabels\Scripts"
-
-USE [$(GraniteDatabase)]
-GO
-
-:r $(path)$(root)\ProcessSteps.sql
-:r $(path)$(root)\PrescriptPrintShipLabelsQty.sql
-:r $(path)$(root)\PrescriptPrintShipLabelsDocument.sql
-:r $(path)$(root)\Label_ShipLabel.sql
