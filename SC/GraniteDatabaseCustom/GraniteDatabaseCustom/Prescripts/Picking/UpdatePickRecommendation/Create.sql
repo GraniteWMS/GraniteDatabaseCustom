d9d5712704ebe2afc167d28b7b58a75559232cc1
craigcoll <craigc@cradle.co.za>
2022-04-29 13:44:08 +0200

    Update Create.sql

--- SC/GraniteDatabaseCustom/GraniteDatabaseCustom/Prescripts/Picking/UpdatePickRecommendation/Create.sql
+++ SC/GraniteDatabaseCustom/GraniteDatabaseCustom/Prescripts/Picking/UpdatePickRecommendation/Create.sql
@@ -14,22 +14,21 @@ DECLARE @valid bit = 1
 DECLARE @message varchar(MAX) = ''
 DECLARE @stepInput varchar(MAX) 
 DECLARE @Document varchar(50)
-DECLARE @UpdateComment varchar(MAX)
+
 
 SELECT @stepInput = Value FROM @input WHERE Name = 'StepInput' 
 
 IF (SELECT Document.[Status] FROM Document WHERE Number = @stepInput) IN ('ENTERED', 'RELEASED')
 BEGIN
 
-	SELECT @UpdateComment = ISNULL('Pick from:' + (SELECT TOP 1  L.Barcode + ' - TE:' +  TE.Barcode
-		FROM TrackingEntity TE Inner Join Location L
-		ON TE.Location_id = L.ID
-		WHERE TE.InStock = 1 AND TE.OnHold = 0 AND TE.Qty > 0 AND TE.MasterItem_id =  DocumentDetail.Item_id AND L.ERPLocation = DocumentDetail.FromLocation 
-		ORDER BY L.[Type],TE.ExpiryDate,TE.CreatedDate), 'ZZZ-No Stock')
-
-	UPDATE DocumentDetail SET Comment = @UpdateComment
+	UPDATE DocumentDetail SET Comment = isnull('Pick from:' + (SELECT TOP 1  L.Barcode + ' - TE:' +  TE.Barcode
+	FROM TrackingEntity TE Inner Join Location L
+	ON TE.Location_id = L.ID
+	WHERE TE.InStock = 1 AND TE.OnHold = 0 AND TE.Qty > 0 AND TE.MasterItem_id=  DocumentDetail.Item_id AND L.ERPLocation = DocumentDetail.FromLocation 
+	ORDER BY L.[Type],TE.ExpiryDate,TE.CreatedDate),'ZZZ-No Stock')
 	FROM DocumentDetail INNER JOIN Document ON Document.ID = DocumentDetail.Document_id
 	WHERE Document.Number = @stepInput
+
 
 	SELECT @message = 'Document pick locations updated'
 	SELECT @valid = 1
