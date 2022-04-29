acac1dfdc0903f9badbc6f63b4f9cb704b72d3aa
fdtaljaard <francoist@cradle.co.za>
2022-04-29 11:33:55 +0200

    Document

--- SC/GraniteDatabaseCustom/GraniteDatabaseCustom/Common/Mail/Create.sql
+++ SC/GraniteDatabaseCustom/GraniteDatabaseCustom/Common/Mail/Create.sql
@@ -10,9 +10,17 @@ GO
 :r $(path)\Common\Mail\Account.sql
 :r $(path)\Common\SqlToHtml\Create.sql
 
+-- Custom for client (Sites)
 :r $(path)$(root)\Mail_JOBCompanyDailyProduction.sql
+
 :r $(path)$(root)\Mail_MOProgress.sql
+
 :r $(path)$(root)\Mail_TransactionsPicked.sql
+
+-- Accpac Table POPORH1
 :r $(path)$(root)\Mail_TransactionsReceiving.sql
+
+-- Accpac Table POPORH1
 :r $(path)$(root)\Mail_TransactionsReceivingReceipt.sql
+
 :r $(path)$(root)\Mail_TransactionsTransfer.sql
