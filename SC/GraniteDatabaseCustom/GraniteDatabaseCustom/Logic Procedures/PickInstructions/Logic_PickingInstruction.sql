304bd0e5c3c66522d394101c4d6c374905e79814
craigcoll <craigc@cradle.co.za>
2022-11-02 02:16:18 +0200

    Update Logic_PickingInstruction.sql

--- SC/GraniteDatabaseCustom/GraniteDatabaseCustom/Logic Procedures/PickInstructions/Logic_PickingInstruction.sql
+++ SC/GraniteDatabaseCustom/GraniteDatabaseCustom/Logic Procedures/PickInstructions/Logic_PickingInstruction.sql
@@ -21,6 +21,8 @@ CREATE PROCEDURE [dbo].[Logic_PickingInstruction]
 AS
 BEGIN
 
+	DECLARE @Document_id BIGINT
+	SELECt @Document_id = ID FROM Document WHERE Number = @Document
 --Simple allocation where there is stock -Show only the TOP Location.
 	UPDATE DocumentDetail SET Instruction = isnull('Pick from:' + (SELECT TOP 1  L.Barcode + ' (OnHand:' + CONVERT(varchar,(CONVERT(int,TE.Qty))) + ')'
 	FROM TrackingEntity TE Inner Join Location L
@@ -29,22 +31,15 @@ BEGIN
 	AND TE.MasterItem_id=  DocumentDetail.Item_id AND L.ERPLocation = DocumentDetail.FromLocation 
 	ORDER BY L.[Type],TE.ExpiryDate,TE.CreatedDate),'ZZZ-No Stock')
 	FROM DocumentDetail INNER JOIN Document ON Document.ID = DocumentDetail.Document_id
-	WHERE Document.Number = @Document
-
---Show multiple locations and batches instead of TrackingEntities -Also include the Batch Qty
+	WHERE Document.ID = @Document_id
 
 
-----Allocation WHERE We show Location and TrackingEntity
---	UPDATE DocumentDetail SET Instruction = isnull('Pick from:' + (SELECT TOP 1  L.Barcode + ' TE:-' + TE.Barcode
---	FROM TrackingEntity TE Inner Join Location L
---	ON TE.Location_id = L.ID
---	WHERE TE.InStock = 1 AND TE.OnHold = 0 AND TE.Qty > 0  and L.NonStock = 0 and L.isActive = 1
---	AND TE.MasterItem_id=  DocumentDetail.Item_id AND L.ERPLocation = DocumentDetail.FromLocation 
---	ORDER BY L.[Type],TE.ExpiryDate,TE.CreatedDate),'ZZZ-No Stock')
---	FROM DocumentDetail INNER JOIN Document ON Document.ID = DocumentDetail.Document_id
---	WHERE Document.Number = @Document
 
-
+	UPDATE DocumentDetail SET LinePriority = TSQL.LinePriority
+	FROM DocumentDetail DD INNER JOIN (SELECT ROW_NUMBER () OVER(ORDER BY Instruction) as LinePriority, LineNumber 
+	FROM DocumentDetail INNER JOIN Document ON Document.ID = DocumentDetail.Document_id
+	WHERE Document.ID = @DocumentID) TSQL ON DD.LineNumber = TSQL.LineNumber
+	WHERE DD.Document_id = @DocumentID
 
 
 END
